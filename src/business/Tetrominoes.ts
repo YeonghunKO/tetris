3b0c66d6604b7663694daff1c7b8e910c6e40610
Sinkyo <yhko1988@gmail.com>
2023-08-02 10:53:06 +0900

    feat : 유난히 이해가 안되는 transferToBoard 였다. 주석 추가

--- src/business/Tetrominoes.ts
+++ src/business/Tetrominoes.ts
@@ -74,19 +74,26 @@ export const randomTetromino = () => {
   return TETROMINOES[key];
 };
 
+// 넘겨받은 rows(board) 에 shape(블록)를 새겨넣는 일
+// shape에서 1로 표시된 부분이 {occupied , className}으로 변환됨
+// 단 position = {row,column} 이 설정되어있으면 shape 에서 1의 위치가 변경됨
+
 export const transferToBoard = ({
-  className,
-  isOccupied,
-  position,
-  rows,
-  shape,
+  className, // shape에서 1로 표되는 부분만 className이 지정됨
+  isOccupied, //
+  position, // shape에서 position만큼 이동시킴, 예를 들어 shape에서 0,0 지점이 1이었다면 position = {row:1,column:1} 라면 1,1지점에 1로 지정됨
+  rows, // pass된 board를 뜻함.
+  shape, // tetromino , 즉 블럭을 뜻함. rows(board)에 shape가 입혀짐
 }: TransferToBoardProps) => {
+  console.log("shape", shape);
+
   shape.forEach((row, y) => {
     row.forEach((cell, x) => {
       if (cell) {
         const occupied = isOccupied;
         const _y = y + position.row;
         const _x = x + position.column;
+
         rows[_y][_x] = { occupied, className };
       }
     });
