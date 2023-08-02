a228e6107e8daaf4663960e811d554ba029a5dbb
Sinkyo <yhko1988@gmail.com>
2023-08-02 14:11:20 +0900

    feat : previews 컴포넌트 완성

--- src/components/Tetris/Previews/Preview.tsx
+++ src/components/Tetris/Previews/Preview.tsx
@@ -1,6 +1,7 @@
 import { buildBoard } from "@/business/Board";
 import { transferToBoard } from "@/business/Tetrominoes";
 import { BuildPlayerProp } from "@/hooks/usePlayer";
+import { BoardCell } from "../Board/BoardCell";
 
 interface IPreview {
   tetromino: BuildPlayerProp;
@@ -17,12 +18,22 @@ export const Preview = ({ index, tetromino }: IPreview) => {
   board.rows = transferToBoard({
     className,
     isOccupied: false,
-    position: { row: 1, column: 1 },
+    position: { row: 0, column: 0 },
     rows: board.rows,
     shape,
   });
 
   console.log("board", board);
 
-  return <div>Preview</div>;
+  return (
+    <div className="Preview">
+      <div className="Preview-board">
+        {board.rows.map((row, y) =>
+          row.map((cell, x) => (
+            <BoardCell key={x * board.size.columns + x} cell={cell} />
+          ))
+        )}
+      </div>
+    </div>
+  );
 };
