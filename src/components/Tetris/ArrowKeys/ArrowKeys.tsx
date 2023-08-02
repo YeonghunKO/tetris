5384149b16ada605668d9af3f008c6fd5b8c6566
Sinkyo <yhko1988@gmail.com>
2023-08-02 14:12:23 +0900

    feat : arrow keys 구현 시작

---
+++ src/components/Tetris/ArrowKeys/ArrowKeys.tsx
@@ -0,0 +1,12 @@
+import Arrows from "@/components/common/icons/Arrows";
+
+const ArrowKeys = () => {
+  return (
+    <div className="Arrows">
+      <Arrows direction="down" />
+      <Arrows direction="up" />
+    </div>
+  );
+};
+
+export default ArrowKeys;
