835f3497545a08ea8f31bba3323d61c323e26a81
Sinkyo <yhko1988@gmail.com>
2023-08-02 10:47:55 +0900

    refactor : 모바일로 열었을시 찌그러지지 않게 하기 위해

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Game } from "@/components/Tetris/Game/Game";
 function App() {
   return (
     <div className="App">
-      <Game rows={20} columns={20} />
+      <Game rows={20} columns={10} />
     </div>
   );
 }
